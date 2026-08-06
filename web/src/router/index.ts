a0a40eaf21c977bbe3c135aadd254d8c0b1cfc9e
pangphanna <pangphanna@mcnc.co.kr>
2026-08-06 07:57:30 +0700

    refactor: switch router history mode from web history to hash history

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router';
+import { createRouter, createWebHashHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import TabsPage from '../views/TabsPage.vue'
 
@@ -208,7 +208,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes
 })
 
